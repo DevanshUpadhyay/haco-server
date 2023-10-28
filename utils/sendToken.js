9177f459dab5ad4735d5dcc5865fe84ead1fcac4
devanshupadhyayfreelance <devanshupadhyayfreelance@gmail.com>
2023-10-28 17:37:01 +0530

    28 Oct 7th

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -14,13 +14,13 @@ export const sendToken = (res, user, message, statusCode = 200) => {
       expires: new Date(
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
+      // httpOnly: true,
+      // secure: true,
+      // sameSite: "none",
     })
     .json({
       success: true,
-      message: token,
+      message,
       user,
     });
 };
